9a9faf26d2b06c358c801e4ada7b3424b744c060
kent0505 <otabekyusupov0550@gmail.com>
2024-10-08 05:20:18 +0500

    settings page

--- lib/core/widgets/custom_scaffold.dart
+++ lib/core/widgets/custom_scaffold.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 
 import '../config/app_colors.dart';
+import '../utils.dart';
+import 'buttons/cuper_button.dart';
 import 'texts/text_r.dart';
 
 class CustomScaffold extends StatelessWidget {
@@ -8,12 +12,16 @@ class CustomScaffold extends StatelessWidget {
     super.key,
     required this.body,
     required this.title,
+    this.subtitle = '',
     this.home = false,
+    this.onSkip,
   });
 
   final Widget body;
   final String title;
+  final String subtitle;
   final bool home;
+  final void Function()? onSkip;
 
   @override
   Widget build(BuildContext context) {
@@ -22,22 +30,82 @@ class CustomScaffold extends StatelessWidget {
       body: Column(
         children: [
           Container(
-            height: 88,
+            height: 80 + getStatusBar(context),
+            padding: EdgeInsets.only(bottom: home ? 14 : 0),
             decoration: const BoxDecoration(
               color: AppColors.main,
             ),
-            child: Column(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                const Spacer(),
-                Row(
-                  children: [
-                    if (home) ...[
-                      const SizedBox(width: 15),
-                      TextR(title, fontSize: 20),
+                if (home) ...[
+                  const SizedBox(width: 14),
+                  TextR(title, fontSize: 20),
+                  const Spacer(),
+                  CuperButton(
+                    onPressed: () {
+                      context.push('/home/settings');
+                    },
+                    minSize: 25,
+                    child: Row(
+                      children: [
+                        SvgPicture.asset('assets/settings.svg'),
+                        const SizedBox(width: 6),
+                        const TextR('Settings', fontSize: 20),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 18),
+                ] else ...[
+                  SizedBox(
+                    width: 56,
+                    child: CuperButton(
+                      onPressed: () {
+                        context.pop();
+                      },
+                      padding: 22,
+                      minSize: 22,
+                      child: const TextR(
+                        '<',
+                        fontSize: 32,
+                        color: AppColors.blue,
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const TextR('Settings', fontSize: 20),
+                      TextR(
+                        subtitle,
+                        fontSize: 15,
+                        color: AppColors.white70,
+                      ),
+                      const SizedBox(height: 8),
                     ],
-                  ],
-                ),
-                const SizedBox(height: 14),
+                  ),
+                  const Spacer(),
+                  SizedBox(
+                    width: 56,
+                    child: onSkip != null
+                        ? Padding(
+                            padding: const EdgeInsets.only(
+                              bottom: 14,
+                              right: 14,
+                            ),
+                            child: CuperButton(
+                              onPressed: onSkip,
+                              child: const TextR(
+                                'Skip',
+                                fontSize: 20,
+                                color: AppColors.blue,
+                              ),
+                            ),
+                          )
+                        : Container(),
+                  ),
+                ],
               ],
             ),
           ),
--- lib/features/home/pages/settings_page.dart
+++ lib/features/home/pages/settings_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../../core/config/app_colors.dart';
+import '../../../core/widgets/buttons/cuper_button.dart';
 import '../../../core/widgets/custom_scaffold.dart';
 import '../../../core/widgets/texts/text_r.dart';
 
@@ -13,14 +16,192 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return const CustomScaffold(
-      title: '',
+    return CustomScaffold(
+      title: 'Settings',
       body: Column(
         children: [
-          TextM(
-            'Settings',
-            fontSize: 16,
+          const SizedBox(height: 11),
+          Container(
+            height: 90,
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            decoration: BoxDecoration(
+              color: AppColors.main,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Row(
+              children: [
+                const SizedBox(width: 15),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 13),
+                      const TextR('Give us your feedback', fontSize: 16),
+                      const SizedBox(height: 7),
+                      TextR(
+                        'Help us improve the app with your opinion',
+                        fontSize: 12,
+                        maxLines: 3,
+                        color: AppColors.white70,
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: SvgPicture.asset('assets/settings2.svg'),
+                  ),
+                ),
+              ],
+            ),
           ),
+          const SizedBox(height: 11),
+          Container(
+            height: 123,
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            decoration: BoxDecoration(
+              color: AppColors.main,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Column(
+              children: [
+                _Tile(
+                  'Terms of use',
+                  onPressed: () {},
+                ),
+                Container(
+                  height: 1,
+                  color: const Color(0xff363636),
+                ),
+                _Tile(
+                  'Privace Policy',
+                  onPressed: () {},
+                ),
+                Container(
+                  height: 1,
+                  color: const Color(0xff363636),
+                ),
+                _Tile(
+                  'Support page',
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 11),
+          Container(
+            height: 123,
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            decoration: BoxDecoration(
+              color: AppColors.main,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Column(
+              children: [
+                _Tile(
+                  'Share with friends',
+                  onPressed: () {},
+                ),
+                Container(
+                  height: 1,
+                  color: const Color(0xff363636),
+                ),
+                _Tile(
+                  'Subscription info',
+                  onPressed: () {},
+                ),
+                Container(
+                  height: 1,
+                  color: const Color(0xff363636),
+                ),
+                const _Tile(
+                  'Send notification',
+                  notification: true,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Tile extends StatefulWidget {
+  const _Tile(
+    this.title, {
+    this.notification = false,
+    this.onPressed,
+  });
+
+  final String title;
+  final bool notification;
+  final void Function()? onPressed;
+
+  @override
+  State<_Tile> createState() => _TileState();
+}
+
+class _TileState extends State<_Tile> {
+  bool active = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return CuperButton(
+      onPressed: () {
+        if (widget.notification) {
+          setState(() {
+            active = !active;
+          });
+        } else {
+          widget.onPressed!();
+        }
+      },
+      minSize: 40,
+      child: Row(
+        children: [
+          const SizedBox(width: 15),
+          TextR(
+            widget.title,
+            fontSize: 16,
+            color: AppColors.white70,
+          ),
+          const Spacer(),
+          if (widget.notification)
+            Container(
+              height: 27,
+              width: 47,
+              decoration: BoxDecoration(
+                color: active ? AppColors.white50 : const Color(0xffB0BEC5),
+                borderRadius: BorderRadius.circular(27),
+              ),
+              child: Stack(
+                children: [
+                  AnimatedPositioned(
+                    duration: const Duration(milliseconds: 400),
+                    top: 2,
+                    left: active ? 22 : 2,
+                    child: AnimatedContainer(
+                      duration: const Duration(milliseconds: 400),
+                      height: 23,
+                      width: 23,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color:
+                            active ? AppColors.main : const Color(0xffECEFF1),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          else
+            TextR(
+              '>',
+              fontSize: 32,
+              color: AppColors.white70,
+            ),
+          SizedBox(width: widget.notification ? 9 : 13),
         ],
       ),
     );
